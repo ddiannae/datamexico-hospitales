19eaa85a244afd87ed84cc53275fbe45ae7b492a
Diana <diana.gco@gmail.com>
2022-07-17 17:06:04 -0500

    Agregando script para descargar shp file

---
+++ 03_getdata_shp.R
@@ -0,0 +1,14 @@
+## Script para descargar shapefile del marco estadístico 2020
+## directamente de la página del INEGI
+
+# Aumentar el timeout para descarga
+options(timeout = max(300, getOption("timeout")))
+
+# Desgarcar shape file
+temp <- tempfile()
+download.file("https://www.inegi.org.mx/contenidos/productos/prod_serv/contenidos/espanol/bvinegi/productos/geografia/marcogeo/889463807469/mg_2020_integrado.zip",
+              temp)
+
+# Guardar archivo
+unzip(temp, files = "conjunto_de_datos/00mun.shp", exdir = "data", 
+      junkpaths = TRUE)
