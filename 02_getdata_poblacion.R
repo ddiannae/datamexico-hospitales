23dd76502a062f71c2bb2ab1897472818533676f
Diana Elisa García Cortés dgarcia@inmegen.edu.mx <dgarcia@inmegen.edu.mx>
2022-07-17 20:33:23 -0500

    Refactor de script para obtener variables demográficas (grupos de edad) para los modelos

--- 02_getdata_poblacion.R
+++ 02_getdata_poblacion.R
@@ -10,24 +10,30 @@ download.file("https://www.inegi.org.mx/contenidos/programas/ccpv/2020/datosabie
 
 # Seleccionar solamente las variables de afiliación a 
 # instituciones de salud
-poblacion <- vroom(unz(temp, "iter_00_cpv2020/conjunto_de_datos/conjunto_de_datos_iter_00CSV20.csv"),
+poblacion_salud <- vroom::vroom(unz(temp, "iter_00_cpv2020/conjunto_de_datos/conjunto_de_datos_iter_00CSV20.csv"),
                   col_select = c(ENTIDAD, NOM_ENT, MUN, NOM_MUN, LOC, NOM_LOC, 
                                  POBTOT, PSINDER, PDER_SS, PDER_IMSS, PDER_ISTE,
                                  PDER_ISTEE, PAFIL_PDOM, PDER_SEGP, PDER_IMSSB,
                                  PAFIL_IPRIV, PAFIL_OTRAI), 
                   col_types = cols(.default = col_character()))
+
+# Seleccionar solamente las variables demográficas
+poblacion_demo <- vroom::vroom(unz(temp, "iter_00_cpv2020/conjunto_de_datos/conjunto_de_datos_iter_00CSV20.csv"),
+                               col_select = c(ENTIDAD, NOM_ENT, MUN, NOM_MUN, LOC, NOM_LOC, 
+                                              POBTOT, POBFEM, POB0_14, POB15_64, POB65_MAS))
+
 unlink(temp)
 
 # Filtrar datos por municipio y generar id igual al
 # identificador utilizado en datamexico
-poblacion <- poblacion %>% 
+poblacion_salud <- poblacion_salud %>% 
   filter(NOM_LOC == "Total del Municipio") %>%
   select(-LOC, -NOM_LOC) %>%
   mutate(ENTIDAD = as.numeric(ENTIDAD), 
          municipality_id = paste0(ENTIDAD, MUN))
 
 # Generar tibble en formato largo
-poblacion <- poblacion %>%
+poblacion_salud <- poblacion_salud %>%
   select(state_id = ENTIDAD, state = NOM_ENT, municipality_id, municipality = NOM_MUN,
          everything(), -MUN) %>%
   pivot_longer(cols = starts_with("P"), names_to = "variable", values_to = "valor") %>%
@@ -41,12 +47,21 @@ dicc <- tibble(variable = c("POBTOT", "PSINDER", "PDER_SS", "PDER_IMSS", "PDER_I
                           "ISSSTE estatal", "PEMEX, SEDENA, SEMAR", "SSA", "IMSS-BIENESTAR", 
                           "SMP", "Otro"))
 
-poblacion <- poblacion %>% inner_join(dicc)
+poblacion_salud <- poblacion_salud %>% inner_join(dicc)
 
 # Acortar nombres de estados
-poblacion[poblacion["state_id"] == 5, "state"] = "Coahuila"
-poblacion[poblacion["state_id"] == 16, "state"] = "Michoacán"
-poblacion[poblacion["state_id"] == 30, "state"] = "Veracruz"
+poblacion_salud[poblacion_salud["state_id"] == 5, "state"] = "Coahuila"
+poblacion_salud[poblacion_salud["state_id"] == 16, "state"] = "Michoacán"
+poblacion_salud[poblacion_salud["state_id"] == 30, "state"] = "Veracruz"
 
 # Guardar datos
-vroom::vroom_write(poblacion, "data/poblacion.tsv")
+vroom::vroom_write(poblacion_salud, "data/poblacion_salud.tsv")
+
+# Seleccionar unicamente los valores por municipio
+poblacion_demo <- poblacion_demo %>% 
+  filter(NOM_LOC == "Total del Municipio") %>%
+  select(-LOC, -NOM_LOC) 
+
+# Guardar datos
+vroom::vroom_write(poblacion_demo, "data/poblacion_demo.tsv")
+
